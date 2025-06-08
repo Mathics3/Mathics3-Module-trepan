74744bc56723a73ad77a1159c4a834fab46689e0
rocky <rb@dustyfeet.com>
2025-06-08 11:52:01 -0400

    Format Python function items better...
    We don't need to include the memory address, but make explicit note that
    this is Python.

--- pymathics/trepan/lib/format.py
+++ pymathics/trepan/lib/format.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#   Copyright (C) 2025 Rocky Bernstein <rocky@gnu.org>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import types
 from mathics.builtin.patterns.basic import Blank, BlankNullSequence, BlankSequence
 from mathics.builtin.patterns.composite import Pattern, OptionsPattern
 from mathics.builtin.patterns.rules import RuleDelayed
@@ -260,6 +277,8 @@ def format_element(
             f"{format_element(element.pattern)} :> "
             "{format_element(element.replace, use_operator_form=use_operator_form)}"
         )
+    elif isinstance(element, types.FunctionType):
+        return f"<Python function {element.__qualname__}>"
     return str(element)
 
 
